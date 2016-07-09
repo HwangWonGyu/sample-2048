1c366a0390dca8de1c5c8b80e22747b62ad14683
HwangWonGyu <hwksdream@naver.com>
2016-07-10 06:35:39 +0900

    블록 이동 - 상하

--- src/main/java/com/hugecomp/sample_2048/service/impl/BlockMoverImpl.java
+++ src/main/java/com/hugecomp/sample_2048/service/impl/BlockMoverImpl.java
@@ -1,6 +1,7 @@
 package com.hugecomp.sample_2048.service.impl;
 
 import com.hugecomp.sample_2048.model.Board;
+import com.hugecomp.sample_2048.model.Table;
 import com.hugecomp.sample_2048.model.enums.Direction;
 import com.hugecomp.sample_2048.service.BlockMover;
 
@@ -19,6 +20,18 @@ public class BlockMoverImpl implements BlockMover {
 				break;
 			default :
 				throw new IllegalArgumentException(direction.name() + " is not valid direction");
+		}
+	}
+	
+	private void moveBlocksVertically(Board[][] blocks, Direction direction) {
+		boolean isUp = (direction == Direction.UP);
+
+		int start = isUp ? 0 : Table.SIZE - 1;
+		int end = isUp ? Table.SIZE : -1;
+		int interval = isUp ? 1 : -1;
+
+		for (int columnIndex = 0; columnIndex < Table.SIZE; columnIndex++) {
+			moveBlocksVerticallyInAColumn(blocks, columnIndex, start, end, interval);
 		}
 	}
 }
