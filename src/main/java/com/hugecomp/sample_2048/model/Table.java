75bc1100230f1798d95babeee5fd10996dafce53
HwangWonGyu <hwksdream@naver.com>
2016-07-10 03:07:19 +0900

    점수 setter, getter

--- src/main/java/com/hugecomp/sample_2048/model/Table.java
+++ src/main/java/com/hugecomp/sample_2048/model/Table.java
@@ -3,12 +3,21 @@ package com.hugecomp.sample_2048.model;
 public class Table {
 	public static final int SIZE = 4;
 	private Board[][] blocks = new Board[SIZE][SIZE];
-
+	private int score = 0;
+	
 	public void setBlocks(Board[][] blocks) {
 		this.blocks = blocks;
 	}
 	
 	public Board[][] getBlocks() {
 		return this.blocks;
+	}
+	
+	public void addScore(int score) {
+		this.score += score;
+	}
+
+	public int getScore() {
+		return this.score;
 	}
 }
