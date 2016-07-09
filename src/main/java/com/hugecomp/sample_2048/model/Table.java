c3102c2b877009006c6f23b402068e926657530b
HwangWonGyu <hwksdream@naver.com>
2016-07-10 03:04:23 +0900

    Table 클래스 setter, getter

---
+++ src/main/java/com/hugecomp/sample_2048/model/Table.java
@@ -0,0 +1,14 @@
+package com.hugecomp.sample_2048.model;
+
+public class Table {
+	public static final int SIZE = 4;
+	private Board[][] blocks = new Board[SIZE][SIZE];
+
+	public void setBlocks(Board[][] blocks) {
+		this.blocks = blocks;
+	}
+	
+	public Board[][] getBlocks() {
+		return this.blocks;
+	}
+}
