dbc3046f333375e16cd82119270cd0cacbb66b5c
HwangWonGyu <hwksdream@naver.com>
2016-07-10 03:18:57 +0900

    블록에 특정값 생성, 빈 블록 검사

--- src/main/java/com/hugecomp/sample_2048/model/Table.java
+++ src/main/java/com/hugecomp/sample_2048/model/Table.java
@@ -20,4 +20,26 @@ public class Table {
 	public int getScore() {
 		return this.score;
 	}
+	
+	public Board createBlockToEmptySpace() {
+
+		Point emptyPoint = getEmptyPointInTable();
+		Board block = Board.createRandomValueToRandomPosition(2, 4);
+		blocks[emptyPoint.getY()][emptyPoint.getX()] = block;
+
+		return block;
+	}
+	
+	private Point getEmptyPointInTable() {
+		int randomFactor = 100;
+
+		while(true) {
+			int row = ((int)(Math.random() * randomFactor)) % SIZE;
+			int col = ((int)(Math.random() * randomFactor)) % SIZE;
+
+			if (this.blocks[row][col] == null) {
+				return new Point(col, row);
+			}
+		}
+	}
 }
