e105f6f86f13a31244b998a32453c7cb365a5e41
HwangWonGyu <hwksdream@naver.com>
2016-07-10 06:56:20 +0900

    TableService 인터페이스 이동 메소드

--- src/main/java/com/hugecomp/sample_2048/service/impl/TableServiceImpl.java
+++ src/main/java/com/hugecomp/sample_2048/service/impl/TableServiceImpl.java
@@ -2,10 +2,14 @@ package com.hugecomp.sample_2048.service.impl;
 
 import com.hugecomp.sample_2048.model.Table;
 import com.hugecomp.sample_2048.model.enums.Direction;
+import com.hugecomp.sample_2048.service.BlockMerger;
+import com.hugecomp.sample_2048.service.BlockMover;
 import com.hugecomp.sample_2048.service.TableService;
 
 public class TableServiceImpl implements TableService {
 	public static final int INITIAL_BLOCK_COUNT = 2;
+	private static BlockMover blockMover = new BlockMoverImpl();
+	private static BlockMerger blockMerger = new BlockMergerImpl();
 	
 	public Table getInitializedTable() {
 		Table table = new Table();
@@ -16,4 +20,14 @@ public class TableServiceImpl implements TableService {
 		return table;
 	}
 
+	public void move(Table table, Direction direction) {
+		blockMover.moveBlocks(table.getBlocks(), direction);
+		int score = blockMerger.mergeBlocksIfAbleToMergeAndReturnScore(table.getBlocks(), direction);
+		blockMover.moveBlocks(table.getBlocks(), direction);
+		table.addScore(score);
+
+		if (table.isContinuable()) {
+			table.createBlockToEmptySpace();
+		}
+	}
 }
