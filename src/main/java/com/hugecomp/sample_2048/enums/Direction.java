1f678868cb560bd92ae93ec23f41591b7a126d71
HwangWonGyu <hwksdream@naver.com>
2016-07-09 23:47:41 +0900

    방향키에 해당되는 입력을 Enum으로 구현

---
+++ src/main/java/com/hugecomp/sample_2048/enums/Direction.java
@@ -0,0 +1,23 @@
+package com.hugecomp.sample_2048.enums;
+
+public enum Direction {
+	UP("U"), DOWN("D"), LEFT("L"), RIGHT("R");
+
+	private String code;
+
+	Direction(String code) {
+		this.code = code;
+	}
+
+	public static Direction getByCode(String code) {
+		Direction[] directions = Direction.values();
+
+		for (Direction direction : directions) {
+			if (direction.code.equalsIgnoreCase(code)) {
+				return direction;
+			}
+		}
+
+		throw new IllegalArgumentException(code + " is not valid argument");
+	}
+}
